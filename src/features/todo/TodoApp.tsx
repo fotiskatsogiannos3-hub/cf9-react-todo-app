66bb7deb2721ef70914999e163c0783d53f24c84
fotiskatsogiannos <fotiskatsogiannos3@gmail.com>
2026-05-21 19:56:03 +0300

    feat(todo): finalize TodoForm component

--- src/features/todo/TodoApp.tsx
+++ src/features/todo/TodoApp.tsx
@@ -1,24 +1,32 @@
 import TodoForm from "./TodoForm.tsx";
 import {useRef} from "react";
 import {useTodos} from "./hooks/useTodos.ts";
+import TodoList from "./TodoList.tsx";
 
 const TodoApp = () => {
     const inputRef = useRef<HTMLInputElement>(null);
-
-    const { todos, addTodo, deleteTodo, editTodo, toggleTodo, clearAll } = useTodos();
+    const { todos, addTodo, deleteTodo,editTodo, toggleTodo, clearAll } = useTodos();
 
     const handleAdd = (text: string) => {
         addTodo(text);
-        inputRef.current?.focus();}
+        inputRef.current?.focus();
+        console.log(todos);
+    }
 
     return (
         <>
             <div className="max-w-sm mx-auto pb-12">
                 <h1 className="text-center text-2xl py-8">To-Do List</h1>
+                <TodoForm onAdd={handleAdd} inputRef={inputRef} />
 
-                <TodoForm  onAdd={handleAdd} inputRef={inputRef} />
+                <TodoList
+                    todos={todos}
+                    onToggle={toggleTodo}
+                    onEdit={editTodo}
+                    onDelete={deleteTodo}
+                />
 
-
+                <TodoStats />
 
             </div>
         </>
